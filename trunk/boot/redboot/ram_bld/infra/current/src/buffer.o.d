c8cd20cac7b934742343ca5e797c0ffe6affc442
fanjianqi168@gmail.com <fanjianqi168@gmail.com@2f871c5b-8fa7-9d33-dff3-2f4c133cafc0>
2011-07-07 06:42:18 +0000

    delete ram rom_bld

--- trunk/boot/redboot/ram_bld/infra/current/src/buffer.o.d
+++
@@ -1,4 +0,0 @@
-src/buffer.o.d:  \
- /home/fanjianqi/atheros_work/reference/trunk_11_6_22/boot/redboot/ecos/packages/infra/current/src/buffer.cxx \
-  /home/fanjianqi/atheros_work/reference/trunk_11_6_22/boot/redboot/ram_bld/install/include/pkgconf/system.h \
-  /home/fanjianqi/atheros_work/reference/trunk_11_6_22/boot/redboot/ram_bld/install/include/pkgconf/infra.h
--- trunk/boot/redboot/rom_bld/install/include/lzma.h
+++
@@ -1,9 +0,0 @@
-#ifndef _LZMA
-#define _LZMA
-
-extern int lzma_decode(unsigned char *inBuffer,unsigned char *outBuffer,
-                unsigned int inLength,unsigned int *outLength );
-
-#endif
-
-
